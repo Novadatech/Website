3b095b1727cc71a498f453976af70d83af277a4b
Novada Tech <admin@novadatech.com.au>
2026-03-16 14:32:24 +1100

    Remove GTM and GA4 tracking scripts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,42 +42,11 @@ export default function RootLayout({
   return (
     <html lang="en" className={inter.variable}>
       <body className="font-sans antialiased">
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-5CL2Q8M8"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
-
         <LeadFormProvider>
           <Navbar />
           <main>{children}</main>
           <Footer />
         </LeadFormProvider>
-
-        {/* Google Tag Manager */}
-        <Script id="gtm" strategy="afterInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5CL2Q8M8');`}
-        </Script>
-
-        {/* Google Analytics GA4 - Direct */}
-        <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-MCFXDP0MHC"
-          strategy="afterInteractive"
-        />
-        <Script id="ga4" strategy="afterInteractive">
-          {`window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-MCFXDP0MHC');`}
-        </Script>
 
         {/* Meta Pixel */}
         <Script id="meta-pixel" strategy="afterInteractive">
